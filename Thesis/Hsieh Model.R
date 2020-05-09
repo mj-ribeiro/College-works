9831c566ca6f9aee8f1eebf6c484f2ca16729afb
Marcos J Ribeiro <mjribeiro@usp.br>
2020-05-09 19:22:56 -0300

    atualizei a pasta

---
+++ Thesis/Hsieh Model.R
@@ -0,0 +1,289 @@
+#======================================================================================
+#                               Hsieh Model
+#======================================================================================
+
+setwd("D:/Git projects/Finance/Finance_R")
+
+#------------------ defining parameters
+
+beta = 0.69
+eta = 0.25
+varphi = 0.25
+theta = 3.44
+rho = 0.19
+kappa = 1/(1- eta)
+i = 7
+r = 27
+gamma1 = gamma(1 - ( (theta*(1-rho)) **(-1) )   * (1 - eta)**(-1)  )    
+phi = c(0.138, 0.174, 0.136, 0.1, 0.051, 0.084, 0.168)
+
+
+
+#--------------------------- s - time spent at school   (eq 14)
+
+sf = function(){
+  s = matrix(0, i, 1)
+  
+  for (c in 1:i){
+    print(c)
+    s[c] = (1 + (1-eta)/ (beta*phi[c]) ) ** (-1)                        
+}
+  return(s)
+}
+
+
+
+#------------------------ h_til
+
+s = sf()
+
+h_tilf = function( ){
+
+  h_til = matrix(0, i, r)
+
+  for (c in 1:i){
+    for (j in 1:r){    
+      h_til[c, j] = ( 1 ** varphi * s[c]**phi[c] * eta**eta )**(1 - eta)**(-1) 
+}
+}
+  return (h_til)
+}
+
+
+#----------------------------------------- Human capital of teachers
+
+
+
+w = runif(i*r, 0, 1)
+tau_w =  runif(i*r, -1, 1)
+tau_h = runif(i*r, -1, 1)
+
+x1 = array( c(tau_w, tau_h, w), dim = c(i, r, 3))
+
+
+
+H_trf = function(x1){
+  s = sf()
+  P = matrix(0, 1, r)
+  Ti = matrix(0, i, r)
+  S = matrix(0, i, 1)
+  N = matrix(0, i, r)
+  H = matrix(0, i, r)
+  
+  for (c in 1:i){
+    for (j in 1:r){
+    
+      S[c] = ( s[c]**phi[c]*eta**eta ) ** kappa
+      P[j] = ( ( ( 1 - colSums(x1[ , ,1])[j] ) / ( ( 1 + colSums(x1[ , ,2])[j] )**eta) ) * ( colSums(x1[ , ,3])[j] ) ) ** (theta - kappa)
+      Ti[c, j] = ( ( 1 - x1[c, j, 1] ) * x1[c, j, 3] ) ** ( theta - kappa +kappa*eta) 
+      N[c, j] = ( 1 + x1[c, j, 2]) ** (eta*theta) 
+      
+      H[c, j] = ( ( Ti[c, j] * S[c] * gamma1 ) / ( N[c, j] * P[j]) ** (1/(1-varphi*kappa)))  
+      
+      H_tr = H[i-1, ]
+
+}
+}
+  return (H_tr)
+}
+
+
+
+
+
+#----------------------------------------- w tilde  (Proposition 1)
+
+
+w_tilf = function(x1){
+  
+  w_til = matrix(0, i, r)
+  
+  for (c in 1:i){
+    for (j in 1:r){
+      w_til[c, j] = ( (1 - x1[c, j, 1]) / ( (1 + x1[c, j, 2]) ** eta) ) * H_tr[j]**varphi * x1[c, j, 3] * s[c]**phi[c] * (1 - s[c]) ** ( (1- eta) /beta )           
+}
+}
+  return (w_til) 
+}
+
+
+
+
+
+#--------------------------- w_r
+
+
+w_til = w_tilf(x1)
+
+w_rf = function(){
+  w_r = w_til**theta
+  w_r = colSums(w_r)
+  return(w_r)
+}
+
+w_r = w_rf()
+
+
+#------------------------------------------ p_ir  (eq 19)
+
+
+
+p_irf = function(){
+    
+  p_ir = matrix(0, i, r)
+  
+  for (c in 1:i){
+    for (j in 1:r){
+      p_ir[c, j] = (( w_til[c, j] )) ** theta / w_r[c]
+    
+}
+}
+  p_i = colSums(p_ir)
+  return (p_ir)
+}  
+
+p_irf()
+
+
+
+#---------------------------------------  W (eq 27)
+
+
+Wf = function(x1){
+  
+  W = matrix(0, i, r)
+  
+  for (c in 1:i){
+    for (j in 1:r){
+      W[c, j] = ((1 - s[c])**(-1/beta))/( 1 - x1[c, j, 1] )*gamma1*eta*(w_r[j])**(1/(theta*(1 - eta)))    
+}
+}
+  return (W)
+
+}
+
+
+
+#--------- Simulated data 
+
+#W_t = rbind(c(0.1, 0.42, 0.33, 0.12), c(0.99, 0.22, 0.154, 0.654))
+
+#p_t = rbind(c(0.122, 0.12, 0.132, 0.109), c(0.212, 0.453, 0.3524, 0.114))
+
+
+
+library(readxl)
+
+W_t = read_excel("W_t.xlsx")
+W_t[1] = NULL
+
+W_t = as.matrix(W_t)
+
+#col = colnames(W_t)
+
+
+p_t = read_excel("p_t.xlsx")
+p_t[1] = NULL
+
+p_t = as.matrix(p_t)
+
+
+#--------------------- OBJECTIVE FUNCTION
+
+
+p_ir = p_irf()
+
+
+
+w = matrix(runif(i*r, 0, 1), nrow = i, ncol = r)
+tau_w = matrix( runif(i*r, -1, 1), nrow = i, ncol = r)
+tau_h = matrix(runif(i*r, -1, 1), nrow = i, ncol = r)
+
+x1 = array( c(tau_w, tau_h, w), dim = c(i, r, 3))
+
+
+obj = function(x1){
+  x1 = array(x1, dim = c(i, r, 3))
+  s = sf()
+  h_til = h_tilf()
+  w_til = w_tilf(x1)
+  w_r = w_rf()
+  p_ir = p_irf()
+  H_tr = H_trf(x1)
+  W = Wf(x1)
+  
+    
+  f1 = matrix(0, i, r)
+  #f2 = matrix(0, i, r)    
+  
+  for (c in 1:i){
+    for (j in 1:r){
+      f1[c, j] = ( (W[c, j] - W_t[c, j]) / W_t[c, j] ) ** 2 + ( (p_ir[c, j] - p_t[c, j])  /  p_t[c, j] ) ** 2
+      #f2[c, j] = ( (p_ir[c, j] - p_t[c, j])  /  p_t[c, j] ) ** 2
+}
+}
+    #d1 = sum(f1)
+    #d2 = sum(f2)
+    D = sum(f1)
+    
+    return(D)
+  
+}
+
+obj(x1)
+
+
+
+#-------------------------- Solve
+
+library('Rsolnp')
+library('optimx')
+
+
+
+res = optim(x1,      #starting values 
+            obj,
+            method = 'Nelder-Mead')   #function to optimize
+           
+
+
+x1==res$par
+
+res$par
+res$value
+
+
+#x1 = array( c(tau_w, tau_h, w), dim = c(i, r, 3))
+
+
+#--------  Bounds
+
+
+lb_tau_w = matrix( rep(-1000, i*r), nrow = i, ncol = r)
+lb_tau_h = matrix( rep(-1000, i*r), nrow = i, ncol = r)
+lb_w = matrix( rep(0, i*r), nrow = i, ncol = r)
+LB = array(c(lb_tau_h, lb_tau_w, lb_w), dim = c(i, r, 3))
+
+
+ub_tau_w = matrix( rep(1000, i*r), nrow = i, ncol = r)
+ub_tau_h = matrix( rep(1000, i*r), nrow = i, ncol = r)
+ub_w = matrix( rep(1000, i*r), nrow = i, ncol = r)
+UB = array(c(ub_tau_h, ub_tau_w, ub_w), dim = c(i, r, 3))
+
+
+
+
+
+
+res = solnp(x1,      #starting values 
+            obj,
+            UB=UB,
+            LB=LB)
+
+
+res$values
+
+res$pars
+
+
+
---
+++ Thesis/Hsieh_model.py
@@ -0,0 +1,428 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 10:18:22 2020
+
+@author: Marcos J Ribeiro
+"""
+# see this: https://pastebin.com/cvYBvW3B
+# The model can be viewed in:  https://drive.google.com/drive/u/0/folders/1MOOHg2woM6B2MCLohTPk-kbIgO5RLbVZ
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math as mt
+from scipy.optimize import minimize
+from math import gamma
+import itertools as itl
+import time
+
+
+
+
+
+#----------------------- Defining parameters
+
+
+
+def par():
+    global beta, eta, varphi, theta, rho, i, r, gamma1, phi, kappa
+    beta = 0.69
+    eta = 0.25
+    varphi = 0.25
+    theta = 3.44
+    rho = 0.19
+    kappa = 1/(1- eta)
+    i = 2
+    r = 4
+    gamma1 = gamma(1 - ( (theta*(1-rho)) **(-1) )   * (1 - eta)**(-1)  )    
+    phi = [0.138, 0.174]
+
+
+
+par()
+gamma1
+
+#------------------------ h_til
+
+
+
+def h_tilf( ):
+    global h_til
+    
+    sf( )
+
+    h_til = np.zeros((i, r))
+    for c in range(i):
+        for j in range(r):    
+            h_til[c, j] = ( 1 ** varphi * s[c]**phi[c] * eta**eta )**(1 - eta)**(-1) 
+
+    return h_til
+
+
+
+
+
+#------------------------ Agregate Human capital   (eq 23)
+
+ 
+#
+#def Hf( ):
+#    taus2()
+#    global H, E, c1, g, b, a 
+#    h_tilf( )
+#    
+#    a = 1/ theta* ( 1 - rho )
+#    b = 1/ ( 1 - eta )
+#    
+#    g = gamma(1 - a * b)
+#                  
+#    H = np.zeros((i, r))
+#    #c1 = np.zeros(i)
+#    c1 = ( 1/1 )**(b / eta)
+#    #E = np.zeros(i)
+#    E = c1 * g
+#    
+#    for c in range(i):               
+#            
+#        #c1[c] = ( 1/p_i[c] )**(b / eta)
+#        
+#            
+#        for j in range(r): 
+#            
+#            H[c, j] = p_ir[c, j]*h_til[c, j] * ( ( (1 - x1[0, c, j])/(1 + x1[1, c, j]) ) * x1[2, c, j] ) ** (eta/(1 - eta)) * E
+#          
+#    return H
+
+
+
+#----------------------------------------- s - time spent at school   (eq 14)
+
+
+
+
+def sf( ):
+    global s
+    s = np.zeros((i, 1))
+    
+    for c in range(i):
+        s[c] = (1+ (1-eta)/ (beta*phi[c]) ) ** (-1)                        
+    return s
+
+sf()
+
+#----------------------------------------- Human capital of teachers
+
+#Numpy parece não permite potências fracionárias de números negativos,
+#mesmo que a potência não resulte em um número complexo.
+
+
+
+
+def H_trf():
+    global H_tr, T, P, S, N, H
+        
+    par( )
+    taus2()    
+    sf()
+    
+    P = np.zeros(r)
+    T = np.zeros((i, r))
+    S = np.zeros(i)
+    N = np.zeros((i, r))
+    H = np.zeros((i, r))
+        
+    for c in range(i):
+        for j in range(r):
+            
+            S[c] = ( s[c]**phi[c]*eta**eta ) ** kappa
+            P[j] = ( ( ( 1 - x1[0].sum(axis=0)[j] ) / ( ( 1 + x1[1].sum(axis=0)[j] )**eta) ) * ( x1[2].sum(axis=0)[j] ) ) ** (theta - kappa)
+            T[c, j] = ( ( 1 - x1[0, c, j] ) * x1[2, c, j] ) ** ( theta - kappa +kappa*eta) 
+            N[c, j] = ( 1 + x1[1, c, j]) ** (eta*theta) 
+            
+            H[c, j] = ( ( T[c, j] * S[c] * gamma1 ) / ( N[c, j] * P[j]) ** (1/(1-varphi*kappa)))  
+
+    H_tr = H[i-1, :]
+    return H_tr
+
+
+
+#---------------- p_ir (other aproach)
+#
+#def pf2():
+#    global ps
+#    
+#    #taus2()
+#    q1 = np.zeros((i, r))
+#    q2 = np.zeros(r)
+#    ps = np.zeros((i, r))
+#    for c in range(i):
+#        for j in range(r):
+#            q1[c, j] = ( ( (1 - x1[0, c, j]) / (1 + x1[1, c, j])**eta ) * (x1[2, c, j]) ) **theta 
+#            q2[j] = ( ( (1 - x1[0].sum(axis=0)[j]) / (1 + x1[1].sum(axis=0)[j] )**eta ) * ( x1[2].sum(axis=0)[j] ) ) ** theta
+#            ps[c, j] = ( (q1[c, j] ) / (q2[j] ) ) 
+#    return ps
+#
+#
+#pf2() 
+#
+#ps.sum(axis=0)
+
+#----------------------------------------- w tilde  (Proposition 1)
+
+
+def w_tilf( ):
+    
+    par( )
+    taus2( )
+    H_trf( )
+    
+    global w_til
+    
+    w_til = np.zeros((i, r))
+    
+    for c in range(i):
+        for j in range(r):
+            w_til[c, j] = ( (1 - x1[0, c, j]) / ( (1 + x1[1, c, j]) ** eta) ) * H_tr[j]**varphi * x1[2, c, j] * s[c]**phi[c] * (1 - s[c]) ** ( (1- eta) /beta )           
+            #w_til[c, j] = ( (1 - x1[0, c, j]) / ( (1 + x1[1, c, j]) ** eta) ) * (np.sign(H_tr[j])* np.abs(H_tr[j])**varphi)  * x1[2, c, j] * s[c]**phi[c] * (1 - s[c]) ** ( (1- eta) /beta )           
+
+    return w_til 
+
+
+
+#------------------------------------------ p_ir  (eq 19)
+    
+        
+def p_irf( ):
+    global p_ir, p_i, w_r
+    w_tilf( )
+        
+    #w_r = np.sign(w_til)*np.abs(w_til)**theta
+    w_r = w_til**theta
+    w_r = w_r.sum(axis = 0) 
+
+     
+    p_ir = np.zeros((i, r))
+    
+    for c in range(i):
+        for j in range(r):
+           # p_ir[c, j] = (( w_til[c, j] )) ** theta / w_r[c]
+             p_ir[c, j] = ((np.sign(w_til[c, j])* np.abs(w_til[c, j])**theta) ) / w_r[j]            
+    #p_i = np.sum(p_ir)
+
+    p_i = np.sum(p_ir, axis =0)  
+    return p_ir
+
+
+for c in range(i):
+        for j in range(r):
+            print(f'p_ir= {(( w_til[c, j] )) ** theta / w_r[c] }')
+
+
+#(np.sign(w_til[0, 1])* np.abs(w_til[0, 1])**theta)
+
+#---------------------------------------  W (eq 27)
+
+
+
+
+def Wf():
+    p_irf( )
+    taus2( )
+    global W
+    W = np.zeros((i, r))
+    
+    for c in range(i):
+        for j in range(r):
+            W[c, j] = ((1 - s[c])**(-1/beta))/( 1 - x1[0, c, j] )*gamma1*eta*(w_r[j])**(1/(theta*(1 - eta)))     #        (eq 27)
+    return W
+
+
+
+#--------- Simulated data 
+
+
+def simul():
+    global W_t, p_t
+    
+    W_t = np.array(([0.1, 0.42, 0.33, 0.12], [0.99, 0.22, 0.154, 0.654]))  
+    
+    p_t = np.array(([0.122, 0.12, 0.132, 0.109], [0.212, 0.453, 0.3524, 0.114]))
+
+
+
+#----------------------- Tau's  & w (TPF)
+
+
+
+
+    
+def taus2():
+    global x1, tau_h, tau_w, w
+    par()
+    
+    
+    tau_h = np.random.uniform(low=-100, high=100, size=(i,r))
+    tau_h[0, :] = 0
+
+    tau_w = np.random.uniform(low=-1, high=1, size=(i,r))
+    tau_w[0, : ] = tau_w[0, 0]
+    
+    w =np.random.uniform(low=0, high=1, size=(i,r))
+    w[:, r-1] = 1
+    
+    x1 = np.array( [tau_w, tau_h, w] )
+    
+    return x1
+   
+
+
+#--------------------- OBJECTIVE FUNCTION
+
+ 
+
+def obj(tau):
+    global D, tau_w, tau_h, w
+    #taus()
+    
+    tau = tau.reshape((3, i, r))
+    tau_w = tau[0]
+    tau_h = tau[1]
+    w = tau[2]
+
+    
+    sf()
+    w_tilf()
+    p_irf()
+    H_trf()
+    Wf()
+    simul()
+    
+    f1 = np.zeros((i, r))
+    f2 = np.zeros((i, r))    
+    
+    for c in range(i):
+        for j in range(r):
+            f1[c, j] = ( (W[c, j] - W_t[c, j]) / W_t[c, j] ) ** 2
+            f2[c, j] = ( (p_ir[c, j] - p_t[c, j])  /  p_t[c, j] ) ** 2
+    d1 = np.sum(f1)
+    d2 = np.sum(f2)
+    D = d1 + d2
+
+    return D
+
+
+
+#----------------------------- OPTIMIZATION Scipy
+
+taus2()
+obj(x1)
+
+
+
+
+
+def calibration(v):
+    
+    global D
+    start = time.time()
+    sol = minimize(obj, x1,  method='Nelder-Mead', options={'maxiter':v})
+    end = time.time()
+            
+    print('\033[1;033m=-'*25)
+    print('{:*^50}'.format('Hsieh Model'))
+    print('=-'*25)
+    print('   ')
+    
+    print('\033[1;033mElapsed time:', (end - start))
+    print('   ')
+        
+
+    print(f'tau_w is given by: \n {np.around(sol.x[0:8].reshape(i, r), decimals=4)}')
+    print('   ')
+
+    print(f'tau_h is given by: \n {np.around(sol.x[8:16].reshape(i, r), decimals=4)}')
+    print('   ')
+
+    print(f'w is given by: \n {np.around(sol.x[16:24].reshape(i, r), decimals=4) }')
+    print('   ')
+
+    print(f'The minimun value to D is: {sol.fun}')
+    print('   ') 
+    
+    print('{:*^50}'.format('End of calibration'))
+
+
+calibration(100)
+
+
+
+#--------------------------- OPTIMIZATION Marcos's Algorithm
+
+
+
+def hsieh(n, t=12):
+    global opt, k
+    opt = [t]
+    k = np.zeros((3, i, r))
+
+    for z in itl.count():
+        
+        if z < n+1:
+            taus2()
+            res = obj(x1)
+            print(z)
+            
+            if res < opt[0]:
+                opt.remove(opt[0])
+                opt.append(res)
+                k = x1
+        else: 
+            break
+
+
+
+
+
+
+
+def calibration2(v, t=12):
+    
+    start = time.time()
+    hsieh(v, t)
+    end = time.time()
+            
+    print('\033[1;033m=-'*25)
+    print('{:*^50}'.format('Hsieh Model'))
+    print('=-'*25)
+    print('   ')
+    
+    print('\033[1;033mElapsed time:', (end - start))
+    print('   ')
+        
+
+    print(f'tau_w is given by: \n {k[0]}')
+    print('   ')
+
+    print(f'tau_h is given by: \n {k[1]}')
+    print('   ')
+
+    print(f'w is given by: \n {k[2]}')
+    print('   ')
+
+    print(f'The minimun value to D is: {opt}')
+    print('   ') 
+    
+    print('{:*^50}'.format('End of calibration'))
+
+
+
+calibration2(100000, 15)
+
+
+
+
+
+
+
+
+###################  END
