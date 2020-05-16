3d91de366a8c3bf9902a37913272553797d6940f
Marcos J Ribeiro <mjribeiro@usp.br>
2020-05-16 01:50:30 -0300

    atualizei a pasta

---
+++ monitoring_macro/Phillips_curve.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 00:14:29 2020
+
+@author: Marcos J Ribeiro
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Question 1
+
+n= 12
+mu = 0.3
+z = 0.4
+theta=0
+un = (mu+z)/1.5
+u = un
+pi = np.zeros(n)
+
+for i in range(0,n):
+    print(i)
+    if i>=2:
+        u = 0.04
+        theta = 1
+    if i>=9:
+        mu = 0.6
+    pi[i] = theta*pi[i-1] + (mu + z) - 1.5*u
+
+plt.plot(pi)
+
+
--- monitoring_macro/inflation.R
+++ monitoring_macro/inflation.R
@@ -4,12 +4,15 @@
 
 setwd("D:/Git projects/college_works/monitoring_macro")
 
+# see : https://www.scielo.br/scielo.php?script=sci_arttext&pid=S0101-41612017000100039
+
 
 library(GetBCBData)
 library('xts')
 
 
-# inflation
+# inflation - Índice nacional de preços ao consumidor-amplo (IPCA)
+
 
 inf = gbcbd_get_series(433, first.date= '1981-01-01', last.date = '2020-04-01',  
                       format.data = "long", be.quiet = FALSE)[ ,1:2]
@@ -26,7 +29,9 @@ dinf = diff(inf)
 colnames(dinf) = 'dinf'
 
 
-# unemployment
+
+# unemployment  - Taxa de desocupação - PNADC
+
 
 des = gbcbd_get_series(24369, first.date= '1981-01-01', last.date = '2020-04-01',  
                        format.data = "long", be.quiet = FALSE)[ ,1:2]
@@ -44,12 +49,26 @@ des = xts(des, order.by = data1)
 
 library(ggplot2)
 
-df = data.frame(des, dinf[index(des)])
+df = data.frame(des, dinf[index(des)],inf[index(des)])
+
+
+data3 = index(inf)
+dfinf = data.frame(data3, inf)
+ggplot(data=dfinf, aes(y=`inf`, x=`data3`), alpha=0.5)+geom_line()
+
+
 
 windows()
-g1 = ggplot(data = df, aes(x = `des`, y =`dinf`), alpha=0.5)
+g1 = ggplot(data = df[1:40,], aes(x = `des`, y =`dinf`), alpha=0.5)
 
-g1 + geom_point(color='blue', size=2) + ggtitle('Phillips Curve')
+g1 + geom_point(color='blue', size=2) + 
+  ggtitle('Phillips Curve') + 
+  geom_smooth(method = 'lm',formula = y~x, color='black') +
+  xlab('Unemployment') +
+  ylab('Rate of change in inflation') +
+  theme(axis.title.x = element_text(colour = 'black', size=13),
+        axis.title.y = element_text(colour = 'black', size=13),
+        plot.title = element_text(hjust = 0.5))
 
 
 
@@ -61,7 +80,10 @@ summary(reg1)
 
 
 
+# arima
 
+reg2 = arima(inf, order=c(1,1,0))
+reg2
 
 
 
