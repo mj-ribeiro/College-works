77a843ec5c3de89072e06031e0da5535be756603
Marcos J Ribeiro <mjribeiro@usp.br>
2020-05-22 15:05:59 -0300

    atualizei a pasta]

--- Thesis/Hsieh Model.R
+++ Thesis/Hsieh Model.R
@@ -12,10 +12,10 @@ varphi = 0.25
 theta = 3.44
 rho = 0.19
 kappa = 1/(1- eta)
-i = 7
-r = 27
+i = 2
+r = 4
 gamma1 = gamma(1 - ( (theta*(1-rho)) **(-1) )   * (1 - eta)**(-1)  )    
-phi = c(0.138, 0.174, 0.136, 0.1, 0.051, 0.084, 0.168)
+phi = c(0.138, 0.174)
 
 
 
@@ -81,14 +81,13 @@ H_trf = function(x1){
       H[c, j] = ( ( Ti[c, j] * S[c] * gamma1 ) / ( N[c, j] * P[j]) ** (1/(1-varphi*kappa)))  
       
       H_tr = H[i-1, ]
-
 }
 }
   return (H_tr)
 }
 
 
-
+H_tr = matrix(1, nrow = i, ncol = r)
 
 
 #----------------------------------------- w tilde  (Proposition 1)
@@ -134,16 +133,13 @@ p_irf = function(){
   
   for (c in 1:i){
     for (j in 1:r){
-      p_ir[c, j] = (( w_til[c, j] )) ** theta / w_r[c]
+      p_ir[c, j] =( ( w_til[c, j] ) ^ theta ) / w_r[j]
     
 }
 }
   p_i = colSums(p_ir)
   return (p_ir)
 }  
-
-p_irf()
-
 
 
 #---------------------------------------  W (eq 27)
@@ -166,27 +162,27 @@ Wf = function(x1){
 
 #--------- Simulated data 
 
-#W_t = rbind(c(0.1, 0.42, 0.33, 0.12), c(0.99, 0.22, 0.154, 0.654))
+W_t = rbind(c(0.1, 0.42, 0.33, 0.12), c(0.99, 0.22, 0.154, 0.654))
 
-#p_t = rbind(c(0.122, 0.12, 0.132, 0.109), c(0.212, 0.453, 0.3524, 0.114))
-
-
-
-library(readxl)
-
-W_t = read_excel("W_t.xlsx")
-W_t[1] = NULL
-
-W_t = as.matrix(W_t)
-
-#col = colnames(W_t)
+p_t = rbind(c(0.122, 0.12, 0.132, 0.109), c(0.212, 0.453, 0.3524, 0.114))
 
 
-p_t = read_excel("p_t.xlsx")
-p_t[1] = NULL
 
-p_t = as.matrix(p_t)
-
+# library(readxl)
+# 
+# W_t = read_excel("W_t.xlsx")
+# W_t[1] = NULL
+# 
+# W_t = as.matrix(W_t)
+# 
+# #col = colnames(W_t)
+# 
+# 
+# p_t = read_excel("p_t.xlsx")
+# p_t[1] = NULL
+# 
+# p_t = as.matrix(p_t)
+# 
 
 #--------------------- OBJECTIVE FUNCTION
 
@@ -195,42 +191,41 @@ p_ir = p_irf()
 
 
 
-w = matrix(runif(i*r, 0, 1), nrow = i, ncol = r)
+w =  matrix(runif(i*r, 0, 1), nrow = i, ncol = r)
 tau_w = matrix( runif(i*r, -1, 1), nrow = i, ncol = r)
-tau_h = matrix(runif(i*r, -1, 1), nrow = i, ncol = r)
+tau_h =  matrix(runif(i*r, -1, 1), nrow = i, ncol = r) 
 
 x1 = array( c(tau_w, tau_h, w), dim = c(i, r, 3))
 
 
+obj(x1)
+
+
 obj = function(x1){
+  
   x1 = array(x1, dim = c(i, r, 3))
+  
   s = sf()
   h_til = h_tilf()
   w_til = w_tilf(x1)
   w_r = w_rf()
   p_ir = p_irf()
-  H_tr = H_trf(x1)
+  #H_tr = H_trf(x1)
   W = Wf(x1)
-  
     
-  f1 = matrix(0, i, r)
-  #f2 = matrix(0, i, r)    
-  
-  for (c in 1:i){
-    for (j in 1:r){
-      f1[c, j] = ( (W[c, j] - W_t[c, j]) / W_t[c, j] ) ** 2 + ( (p_ir[c, j] - p_t[c, j])  /  p_t[c, j] ) ** 2
-      #f2[c, j] = ( (p_ir[c, j] - p_t[c, j])  /  p_t[c, j] ) ** 2
-}
-}
-    #d1 = sum(f1)
-    #d2 = sum(f2)
-    D = sum(f1)
+    g = ( (W - W_t)/ W_t ) ^ 2 + ( (p_ir - p_t)/p_t ) ^ 2
+    D = sum(g)
     
     return(D)
-  
 }
 
+
+
 obj(x1)
+
+
+sum(g)
+
 
 
 
@@ -256,6 +251,7 @@ res$value
 #x1 = array( c(tau_w, tau_h, w), dim = c(i, r, 3))
 
 
+
 #--------  Bounds
 
 
@@ -271,14 +267,24 @@ ub_w = matrix( rep(1000, i*r), nrow = i, ncol = r)
 UB = array(c(ub_tau_h, ub_tau_w, ub_w), dim = c(i, r, 3))
 
 
+eeq = function(x1){
+  x1[1, ,1] = x1[1, 1, 1]
+  x1[1, ,2] = 0   # tau_h
+  x1[ ,r,3] = 1   # w
+  
+}
 
 
 
 
-res = solnp(x1,      #starting values 
+
+res = solnp(x1,      
             obj,
             UB=UB,
             LB=LB)
+
+
+
 
 
 res$values
