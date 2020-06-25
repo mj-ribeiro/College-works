ee78e29ae0b23e4b0d6c4aec5935e1af260f0e9d
Marcos J Ribeiro <mjribeiro@usp.br>
2020-06-24 22:09:54 -0300

    atualizei a pasta

--- Bayesian Simulation/MC_Integral.R
+++ Bayesian Simulation/MC_Integral.R
@@ -14,17 +14,21 @@ t_max = 300
 t_min = 0
 n = 500000
 
-s = 0
+nn = 100
+I = c()
 
-for (i in 1:n) {
-  theta = runif(1, min=t_min, max=t_max)
-  f_ = f(2, 1/3)
-  s = s + f_
+for (j in 1:nn) {
+  s = 0
+  for (i in 1:n) {
+    theta = runif(1, min=t_min, max=t_max)
+    f_ = f(2, 1/3)
+    s = s + f_
+  }
+
+  I[j] = (t_max - t_min)/n *s
 }
 
 
-I = (t_max - t_min)/n *s
-
-
-
+mean(I)
+sd(I)
 
