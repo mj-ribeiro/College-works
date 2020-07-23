4ebf50361771080813589711b7dcaf0ea875806f
Marcos J Ribeiro <mjribeiro@usp.br>
2020-07-22 21:42:29 -0300

    atualizei a pasta

--- Thesis/avaliation.py
+++ Thesis/avaliation.py
@@ -65,8 +65,10 @@ W_t = W_t.values.flatten()
 
 # plots
 
-fig, ax = plt.subplots(1, 2, figsize=(16,8))
+fig, ax = plt.subplots(1, 2, figsize=(12,6))
 ax[0].scatter(p_ir, p_t, s=0)
+ax[0].xaxis.set_tick_params(labelsize=20)
+ax[0].yaxis.set_tick_params(labelsize=20)
 for i, txt in enumerate(names):
     ax[0].annotate(txt, (p_ir[i], p_t[i]), size=20) 
 ax[0].set_xlabel('p_ir Model', fontsize=20)
@@ -74,6 +76,8 @@ ax[0].set_ylabel('p_ir PNAD Data', fontsize=20)
 ax[0].plot([0, 0.5], [0, 0.5], 'k-', lw=2)
 ax[0].grid(True)
 ax[1].scatter(W, W_t, s=0)
+ax[1].xaxis.set_tick_params(labelsize=20)
+ax[1].yaxis.set_tick_params(labelsize=20)
 for i, txt in enumerate(names):
     ax[1].annotate(txt, (W[i], W_t[i]), size=20) 
 ax[1].set_xlabel('W_ir Model', fontsize=20)
@@ -83,6 +87,34 @@ ax[1].grid(True)
 plt.tight_layout()    
 
  
+
+
+
+
+## tpf and GDP plots
+
+Y = Y_f(z1)
+tpf = z1[2].sum(axis=0) 
+names2 = n.columns.str.strip("'")
+
+
+
+plt.scatter(tpf, Y, s=10)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+(m, b) = np.polyfit(tpf, Y, 1)
+yp = np.polyval([m, b], tpf)
+plt.plot(tpf, yp, label='Regression Line')
+for i, txt in enumerate(names2):
+    plt.annotate(txt, (tpf[i], Y[i]), size=20) 
+plt.grid(True)
+plt.legend(loc="lower left", prop={'size': 20})
+plt.xlabel("Log of TFP - model", fontsize=20)
+plt.ylabel("GDP per worker - model", fontsize=20)
+plt.tight_layout()    
+
+
+
 
 
 # z1
@@ -96,27 +128,5 @@ tau_h.columns = names
 
 w = pd.DataFrame( z1.reshape(3, i, r)[2] )
 w.columns = names
-
-
-
-
-
-Y = Y_f(z1)
-tpf = np.log(z1[2].sum(axis=0) )
-names2 = n.columns.str.strip("'")
-
-
-plt.scatter(Y, tpf, s=10)
-(m, b) = np.polyfit(Y, tpf, 1)
-yp = np.polyval([m, b], Y)
-plt.plot(Y, yp, label='Regression Line')
-for i, txt in enumerate(names2):
-    plt.annotate(txt, (Y[i], tpf[i]), size=20) 
-plt.grid(True)
-plt.legend(loc="lower left", prop={'size': 20})
-plt.xlabel("Log of TFP - model", fontsize=20)
-plt.ylabel("GDP per worker - model", fontsize=20)
-plt.tight_layout()    
-
 
  
