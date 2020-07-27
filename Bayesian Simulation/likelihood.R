57c6fa09af55f329d6baba90cb0784f1f4984640
Marcos J Ribeiro <mjribeiro@usp.br>
2020-07-26 22:21:17 -0300

    atualizei a pasta

--- Bayesian Simulation/likelihood.R
+++ Bayesian Simulation/likelihood.R
@@ -53,41 +53,14 @@ x2 = c(3, 6, 12, 3, 1.4, 0.9, -1, 22, 4.3)
 
 
 
-
-sig2 = sd(y2)
-T = length(x2)
-
-beta1 = 1
-beta2 = 0.2
-B = rbind(beta1, beta2)
-
+mv_reg = function(y2, x2){
   
-g1 = 1/(T*sig2) *sum(y2 - beta1 - beta2*x2)
-h1 = -1/(sig2)
-
-
-
-g2 = 1/(T*sig2) * sum(y2*x2 - beta1*x2 - beta2*x2^2) 
-h2 = -1/(T*sig2) * sum(x2^2)
-
-
-
-G = rbind(g1, g2)
-
-H = matrix(c(h1, 0, 0, h2), nrow = 2, ncol = 2)
-
-
-tol = 1/1e4
-
-l = 0.1
-
-
-while (tol < max(G)){
-  beta1 = B[1]
-  beta2 = B[2]
+  sig2 = sd(y2)
+  T = length(x2)
   
-  L = 1/2*log(2/pi) - 1/2*log(sig2) - 1/(2*sig2*T)*sum( (y2 - beta1 - beta2*x2)^2)
-  
+  beta1 = 1
+  beta2 = 0.2
+  B = rbind(beta1, beta2)
   
   g1 = 1/(T*sig2) *sum(y2 - beta1 - beta2*x2)
   h1 = -1/(sig2)
@@ -96,20 +69,48 @@ while (tol < max(G)){
   h2 = -1/(T*sig2) * sum(x2^2)
   
   G = rbind(g1, g2)
-  
   H = matrix(c(h1, 0, 0, h2), nrow = 2, ncol = 2)
-
-  B = B - l*solve(H)%*%G
   
-  cat('G:', G, 'L:', L, '\n')
   
+  tol = 1/1e4
+  l = 0.1
+  
+  while (tol < max(G)){
+    beta1 = B[1]
+    beta2 = B[2]
+    
+    L = 1/2*log(2/pi) - 1/2*log(sig2) - 1/(2*sig2*T)*sum( (y2 - beta1 - beta2*x2)^2)
+    
+    
+    g1 = 1/(T*sig2) *sum(y2 - beta1 - beta2*x2)
+    h1 = -1/(sig2)
+    
+    g2 = 1/(T*sig2) * sum(y2*x2 - beta1*x2 - beta2*x2^2) 
+    h2 = -1/(T*sig2) * sum(x2^2)
+    
+    G = rbind(g1, g2)
+    
+    H = matrix(c(h1, 0, 0, h2), nrow = 2, ncol = 2)
+  
+    B = B - l*solve(H)%*%G
+    
+    cat('G:', G, 'L:', L, '\n')
+    
+  }
+  cat('\n')
+  cat('The gradient is given by:\n', G)
+  cat('\n Hessian is given by: \n', H)
+  cat('\n The parameters are given by: \n', B)
 }
 
 
 
+mv_reg(y, x)
 
-reg2 = lm(y2~x2)
 
+
+reg2 = lm(y~x)
+reg2
 
 
 
