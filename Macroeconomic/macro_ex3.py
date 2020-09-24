a29ae62a2209b3924de7b89e2528d4ee4fa2576b
Marcos J Ribeiro <mjribeiro@usp.br>
2020-09-24 19:23:35 -0300

    atualizei a pasta

--- Macroeconomic/macro_ex3.py
+++ Macroeconomic/macro_ex3.py
@@ -4,6 +4,7 @@ Created on Mon Aug 31 15:26:22 2020
 
 @author: Marcos J Ribeiro
 """
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,7 +25,7 @@ def pars():
     pi = np.array(([0.4, 0.5, 0.1],
                    [0.3, 0.2, 0.5],
                    [0.2, 0.4, 0.4]))
-    barA, a_max, n_a = 0, 5, 5
+    barA, a_max, n_a = 0, 5, 3
     a_grid = np.linspace(barA, a_max, n_a)
     
     y_min, y_max, n_y = 1, 3, 3
@@ -54,14 +55,27 @@ def obj_f():
                     obj[i_y, i_a, i_aa] = util(c) + beta*np.dot(pi[i_y,:], V[:,i_aa]) 
     return np.around(obj, 1)
 
+
 ## Tv , iG
 
 def Tv_f(obj):
-    Tv = np.amax(obj, axis=1)
-    Tg = np.argmax(obj, axis=1)
+    Tv = np.zeros((n_y, n_a))
+    Tg = np.zeros((n_y, n_a), dtype=np.int)
+    
+    for i_y in range(n_y):
+        for i_a in range(n_a):
+    
+            Tv[i_y, i_a] = np.max(obj[i_y, i_a])
+            Tg[i_y, i_a] = np.argmax(obj[i_y, i_a])
+            
     return Tv, Tg
 
+obj_f()
+
 Tv_f(obj_f())
+
+
+a_grid
 
 
 ## Solver
@@ -79,9 +93,17 @@ def solve():
 #        print(f'\033[1;033m norma={norma}' )
     return V, iG
 
+ 
+V, iG = solve()
+
+ 
 
 ##  Q function
 
+    # States values
+    # [ (y_0, a_0), (y_0, a_1), (y_0, a_2),
+    #   (y_1, a_0), (y_1, a_1), (y_1, a_2), 
+    #   (y_2, a_0), (y_2, a_1), (y_2, a_2) ]
 
 def M_f(iG):
     M = np.zeros((n_y*n_a, n_y*n_a))
@@ -89,22 +111,41 @@ def M_f(iG):
         for i_a in range(n_a):
             c_state = i_y*n_a + i_a
             for i_yy in range(n_y):
-                for i_aa in range(n_a):
+                for i_aa, aa in enumerate(a_grid):
                     n_state = i_yy*n_a + i_aa
-                    if iG[i_y, i_a] == i_aa:  
+                    if a_grid[iG[i_y, i_a] ] == aa:  
                         M[c_state, n_state] = pi[i_y, i_yy]
     return M
 
+M = M_f(iG)
+M
 
- 
+M_100 = M
+
+for i in range(100):
+    
+    M_100 = np.dot(M_100, M)
+    
+M_100[1,:]
+
+
+np.sum(M_f(iG), axis=1) 
+
+a_grid[iG]
+
+a_grid[iG] == a_grid
+
+
+
 
 ## BarM
 
 def barM_f(M):
     barM  = np.ones(n_a*n_y)/(n_a*n_y)
     norma_m, tol_m = 1, 1e-6
+    
     while norma_m>tol_m:    
-        TbarM = np.dot(M, barM)
+        TbarM = np.dot(barM, M)
         norma_m = np.max(abs(TbarM-barM))
         barM = np.copy(TbarM)
     return barM
@@ -112,11 +153,13 @@ def barM_f(M):
 
 M = M_f(iG)
 
-barM_f(M)
+np.around(barM_f(M), 3) == np.around(M_100[1,:], 3)
 
 
+barM = barM_f(M)
 
-## Poupança agregada
+
+## Poupança agregada média da economia
 
 def poup_f():
     Ea = 0 
@@ -126,7 +169,7 @@ def poup_f():
             poup = a_grid[s_ind]
             t_ind = i_y*n_a + i_a
             size = barM[t_ind]
-            Ea += poup*size
+            Ea += poup*size     #
     return Ea
 
 
@@ -147,6 +190,7 @@ def L_f():
 
 
 # solve 
+ 
 
 
 def main(r):
@@ -157,10 +201,10 @@ def main(r):
     M = M_f(iG)
     barM = barM_f(M)
     
-    Ea = poup_f()
-    L = L_f()
+    Ea = poup_f()  # poup. agregada média da economia
+    L = L_f()      # oferta de trabalho  
         
-    K = k*L    
+    K = k*L       # demanda de capital da firma
     d = abs(K - Ea)
     return d
 
@@ -168,18 +212,13 @@ def main(r):
 from scipy.optimize import minimize
 
 
-sol = minimize(main, 0.5, method='Nelder-Mead')
-
-
-
-sol.x
-sol.fun
-
+sol = minimize(main, 5, method='BFGS')
 
 ## bissec  - My algorithm
 
 
 def main2(r):
+    global w
     k = ( alfa/(r+gamma) )**(1/(1-alfa))
     w = (1-alfa)*( k**alfa )
     
@@ -194,11 +233,11 @@ def main2(r):
     d = K - Ea
     return d
 
-
+ 
 
 pars()
 norma2, tol2 = 1.0, 1e-6
-r_l, r_h = 0, 10
+r_l, r_h = 0, 70
 
 while norma2>tol2:
     r_bar = (r_l+r_h)*0.5
@@ -206,12 +245,15 @@ while norma2>tol2:
     if main2(r_bar)>0:
         r_l = r_bar    
     else:
-        r_h = r_bar
+        r_h = r_bar  
+        
     norma2 = abs(r_l - r_h)
     print(f'norma2 = {norma2}')
     
 
-main2(r_h)
+
+main2(r_l)
+
 
  
  
@@ -219,10 +261,10 @@ main2(r_h)
 # plots
 
 
-r_grid = np.linspace(0, 1.5,100 )
+r_grid = np.linspace(0, 70,50)
 d_grid = main2(r_grid)
 
-
+ 
 plt.plot(d_grid, r_grid)
 plt.plot(sol.fun, sol.x, marker='o', color='red', markersize=20)
 plt.xlabel('Distância', fontsize=20)
